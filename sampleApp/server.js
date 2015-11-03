c637d5e4f21fa67de029a7e1591c22c019aaf041
IvanDobrovolsky <dobrovolsky94@gmail.com>
2015-11-03 18:34:34 +0200

    Added adminRouter middleware which logs request parameters to the console.

--- sampleApp/server.js
+++ sampleApp/server.js
@@ -14,11 +14,23 @@ app.get('/', function (req, res) {
 //Get an instance of the router
 var adminRouter = express.Router();
 
+
+//Route Middleware that will happen on every request
+adminRouter.use(function (req, res, next) {
+
+    //Log each request to the console
+    console.log(req.method, req.url);
+
+    //Continue doing what we were doing and go to the router
+    next();
+});
+
+
+
 //Admin main page. The dashboard (http://localhost:1337/admin)
 adminRouter.get('/', function (req, res) {
     res.send("I am the dashboard!");
 });
-
 
 //Users page (http://localhost:1337/users)
 adminRouter.get('/users', function (req, res) {
@@ -32,6 +44,10 @@ adminRouter.get('/posts', function (req, res) {
 
 //Apply the routes to our application
 app.use('/admin', adminRouter);
+
+
+
+
 
 
 //Start the server
